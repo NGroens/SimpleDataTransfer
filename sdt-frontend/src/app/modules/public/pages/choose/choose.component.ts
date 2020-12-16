25f80292aac8e706590ea9c6cf941515b60db307
NGroens <git@mycoding.systems>
2020-12-16 23:03:56 +0100

    Add responsive design for desktop

--- sdt-frontend/src/app/modules/public/pages/choose/choose.component.ts
+++ sdt-frontend/src/app/modules/public/pages/choose/choose.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterContentInit, Component, OnInit } from '@angular/core';
+import { ResponsiveService } from '../../../../core/services/responsive.service';
 
 @Component({
   selector: 'app-choose',
   templateUrl: './choose.component.html',
   styleUrls: ['./choose.component.scss']
 })
-export class ChooseComponent implements OnInit {
+export class ChooseComponent implements OnInit, AfterContentInit {
 
-  constructor() { }
+  constructor(
+    public responsiveService: ResponsiveService
+  ) {
+  }
 
   ngOnInit(): void {
+  }
+
+  ngAfterContentInit(): void {
+    this.responsiveService.update();
+    setTimeout(() => {
+      this.responsiveService.update();
+
+    }, 4000);
   }
 
 }
